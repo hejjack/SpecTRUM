28dacee67de962d924caa34f7884316d02f6ccb4
hejjack <xhajek9@apollo.fi.muni.cz>
2023-09-09 01:31:26 +0200

    reorganize

--- configs/run_predict.sh
+++
@@ -1,3 +0,0 @@
-python predict.py --checkpoint ./checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1680000/ \
-                  --output-folder predictions \
-                  --config-file configs/predict_config.yaml 
--- configs/run_train_debug.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 python ../train_bart.py --config-file train_config_debug.yaml \
-                                               --checkpoint ../checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1670000 \
-                                               --additional-info _test_new_tokenizer \
-                                               --device cpu \
-                                               --wandb-group debug     # finetune
-                                               # --checkpoints_dir ../checkpoints \
-                                               # --resume-id 2yqhwcas
