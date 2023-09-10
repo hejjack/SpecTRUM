e1a9352d8cdca037f8c3223fa6267768c71e584c
hejjack <hejjack1@gmail.com>
2023-09-10 17:11:35 +0200

    add configs

--- config_runners/run_pretrain_rassp.sh
+++ config_runners/run_pretrain_rassp.sh
@@ -1,11 +1,12 @@
 # RASSP pretrain from scratch
 CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp.yaml \
                                                --additional-info "_rassp_scratch" \
+                                               --additional-tags "A100:scratch" \
                                                --wandb-group pretrain \
-                                               --additional-tags "A100:scratch"
                                             #    --device cpu \
+                                             #   --resume-id qa0wkyd9
+                                             #   --checkpoint ../checkpoints/pretrain/glamorous-dragon-222_rassp_scratch/checkpoint-4000 \
                                                # --checkpoints_dir ../checkpoints \
-                                             #   --checkpoint ../checkpoints/finetune/solar-sponge-191/checkpoint-34272 \
 
 
 # ../checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1680000  # 30M_best
