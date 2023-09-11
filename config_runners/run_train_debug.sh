a0c1060a734ec7491fe993cfb02a0d8d88711f46
hejjack <hejjack1@gmail.com>
2023-09-11 14:22:04 +0200

    add config

--- config_runners/run_train_debug.sh
+++ config_runners/run_train_debug.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_debug.yaml \
+CUDA_VISIBLE_DEVICES=3 python ../train_bart.py --config-file ../configs/train_config_debug.yaml \
                                                --checkpoint ../checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1670000 \
                                                --additional-info _test_apollo \
                                                --device cuda \
