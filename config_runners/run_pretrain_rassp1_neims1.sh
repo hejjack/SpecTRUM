3e3a8d1146e784c1c67dd735e6d3470bb860b643
Adam Hajek <hejjack1@gmail.com>
2023-09-26 23:38:21 +0200

    edit configs

--- config_runners/run_pretrain_rassp1_neims1.sh
+++ config_runners/run_pretrain_rassp1_neims1.sh
@@ -1,12 +1,9 @@
 # RASSP pretrain from scratch
-CUDA_VISIBLE_DEVICES=2 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1.yaml \
+CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1.yaml \
                                                --additional-info "_rassp1_neims1" \
                                                --additional-tags "A100:scratch" \
+                                               --resume-id pwrbt7zt \
                                                --wandb-group pretrain \
-                                            #    --device cpu \
-                                             #   --resume-id qa0wkyd9
-                                             #   --checkpoint ../checkpoints/pretrain/glamorous-dragon-222_rassp_scratch/checkpoint-4000 \
-                                               # --checkpoints_dir ../checkpoints \
-
+                                               --checkpoint ../checkpoints/pretrain/northern-cloud-230_rassp1_neims1/checkpoint-112000 \
 
 # ../checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1680000  # 30M_best
