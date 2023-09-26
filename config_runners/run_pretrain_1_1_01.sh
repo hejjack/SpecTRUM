04eb25ca1430ffe9cde0da7457a75ce8317f535b
Adam Hajek <hejjack1@gmail.com>
2023-09-26 23:50:10 +0200

    add config

--- config_runners/run_pretrain_1_1_01.sh
+++ config_runners/run_pretrain_1_1_01.sh
@@ -1,6 +1,7 @@
 # pretrain RASSP_1 NEIMS_1 NIST_0.1 on 2 A40 on meta
-CUDA_VISIBLE_DEVICES=1 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_nist01.yaml \
-                                               --checkpoint ../checkpoints/pretrain/fallen-star-250_rassp1_neims1_neims01/checkpoint-40000 \
+# CUDA_VISIBLE_DEVICES=1 # META
+python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_nist01.yaml \
+                                               --checkpoint ../checkpoints/pretrain/fallen-star-250_rassp1_neims1_neims01/checkpoint-104000 \
                                                --additional-info "_rassp1_neims1_neims01" \
                                                --additional-tags "scratch" \
                                                --wandb-group pretrain \
