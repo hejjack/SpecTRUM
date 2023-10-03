ff584aa248efc0ec20db227124025512ac079965
hejjack <hejjack1@gmail.com>
2023-10-04 01:43:25 +0200

    update configs

--- config_runners/meta_runner.sh
+++ config_runners/meta_runner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -N BARTSpektro_experiment
 #PBS -q gpu@cerit-pbs.cerit-sc.cz
-#PBS -l select=1:ncpus=5:mem=70gb:scratch_local=1gb:ngpus=2:gpu_cap=cuda80:cluster=^fer
+#PBS -l select=1:ncpus=6:mem=70gb:scratch_local=1gb:ngpus=2:gpu_cap=cuda80:cluster=^fer
 #PBS -l place=scatter
 #PBS -m ae
 
 cd /storage/brno2/home/ahajek/Spektro/MassGenie/config_runners
 source /storage/brno2/home/ahajek/miniconda3/bin/activate BARTtrain
 echo $CONDA_PREFIX
-./run_pretrain_1_1_01.sh
+./run_finetune_4_8M_neims_gen.sh
