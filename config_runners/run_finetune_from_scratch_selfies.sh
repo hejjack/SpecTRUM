a020fff380b75ff3ec9a19899a6e9f1a9f6c5418
Adam Hajek <hejjack1@gmail.com>
2023-10-08 15:18:32 +0200

    delete  <nist> from configs

---
+++ config_runners/run_finetune_from_scratch_selfies.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_finetune_selfies_DEBUG.yaml \
+                                               --additional-info _from_scratch_selfies \
+                                               --wandb-group finetune \
+                                               --additional-tags "A100:alfa:selfies"
