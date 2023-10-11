1bf6e622bc9e50f71c748bd7d52437d67e037d55
Adam Hajek <hejjack1@gmail.com>
2023-10-11 11:48:33 +0200

    add configs for selfies

--- config_runners/run_finetune_from_scratch.sh
+++ config_runners/run_finetune_from_scratch.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_finetune.yaml \
-                                               --additional-info _from_scratch \
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_finetune.yaml \
+                                               --additional-info _from_scratch_untied \
                                                --wandb-group finetune \
-                                               --additional-tags "A100:apollo"
+                                               --additional-tags "A100:alfa"
