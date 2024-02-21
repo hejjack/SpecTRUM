7ae62d3b0e1259406d488583c63d8adea071cd49
Adam Hajek <hejjack1@gmail.com>
2024-02-21 12:07:33 +0100

    move data configs and runners to the common folder

--- data/configs/run_rassp_preprocess.sh
+++
@@ -1,4 +0,0 @@
-python msp_preprocess_rassp.py --input-dir datasets/30M_rassp/rassp_gen/msps \
-                                     --output-dir datasets/30M_rassp/rassp_gen/jsonls \
-                                     --num-processes 64 \
-                                     --concat \
