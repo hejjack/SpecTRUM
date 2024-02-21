60ebdae035aaf8b77f80e40d8021840d3947fb53
Adam Hajek <hejjack1@gmail.com>
2024-02-21 16:22:32 +0100

    update configs

--- config_runners/run_pretrain_neims.sh
+++ config_runners/run_pretrain_neims.sh
@@ -1,14 +1,6 @@
 
-# NEIMS pretrain from scratch
 CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_pretrain_neims.yaml \
-                                               --additional-info "_neims_scratch2_validation_w_neims_token" \
+                                               --additional-info "_neims_scratch" \
+                                               --additional-tags "A100:NEIMS:scratch:alfa" \
                                                --wandb-group pretrain \
-                                               --additional-tags "A100:scratch:apollo:neims_token_valid" \
-                                            #    --device cpu \
-                                               # --checkpoints_dir ../checkpoints \
-
-
-                                             #   --resume-id lw7cyi1d \
-                                             #   --checkpoint ../checkpoints/pretrain/misunderstood-voice-218_neims_scratch/checkpoint-74000 \
-# ../checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1680000  # 30M_best
 
--- config_runners/run_pretrain_rassp.sh
+++ config_runners/run_pretrain_rassp.sh
@@ -1,12 +1,4 @@
-# RASSP pretrain from scratch
-CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp.yaml \
+CUDA_VISIBLE_DEVICES=1 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp.yaml \
                                                --additional-info "_rassp_scratch" \
-                                               --additional-tags "A100:scratch" \
+                                               --additional-tags "A100:RASSP:scratch:alfa" \
                                                --wandb-group pretrain \
-                                            #    --device cpu \
-                                             #   --resume-id qa0wkyd9
-                                             #   --checkpoint ../checkpoints/pretrain/glamorous-dragon-222_rassp_scratch/checkpoint-4000 \
-                                               # --checkpoints_dir ../checkpoints \
-
-
-# ../checkpoints/bart_2023-04-07-18_27_23_30Mneims/checkpoint-1680000  # 30M_best
