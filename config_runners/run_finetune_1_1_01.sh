92a9c10598f36d048e7df38e89301a935e0196da
Adam Hajek <hejjack1@gmail.com>
2024-02-29 01:42:52 +0100

    update configs

--- config_runners/run_finetune_1_1_01.sh
+++ config_runners/run_finetune_1_1_01.sh
@@ -1,6 +1,6 @@
 # finetune model ran on 1-1-0.1 dataset (NEIMS1, RASSP1, NIST01)
-CUDA_VISIBLE_DEVICES=3 python ../train_bart.py --config-file ../configs/train_config_finetune.yaml \
-                                               --checkpoint ../checkpoints/pretrain/fallen-star-250_rassp1_neims1_neims01/checkpoint-112000 \
+CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_finetune_mf10000000.yaml \
+                                               --checkpoint ../checkpoints/pretrain/fortuitous-monkey-376_rassp1_neims1_neims01/checkpoint-112000 \
                                                --additional-info "_rassp1_neims1_nist01" \
                                                --wandb-group finetune  \
                                                --additional-tags "alfa:A100:1-1-0.1" \
--- config_runners/run_pretrain_rassp1_neims1_mf10.sh
+++ config_runners/run_pretrain_rassp1_neims1_mf10.sh
@@ -1,8 +1,14 @@
 # RASSP1 NEIMS1 pretrain from scratch
-python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_mf10.yaml \
+CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_mf10.yaml \
                                                --additional-info "_rassp1_neims1_mf10" \
                                                --additional-tags "A100:scratch:RASSP1NEIMS1:meta" \
                                                --wandb-group pretrain \
+                                               --resume-id 9b1ifnhd \
+                                               --checkpoint ../checkpoints/pretrain/desert-feather-378_rassp1_neims1_mf10/checkpoint-88000
+                                        
+
+
+
 
 
 
