b6be2c48f5cd049c985d61dee39c9eadb0cdf5e5
hejjack <hejjack1@gmail.com>
2024-03-27 12:28:33 +0100

    add NClike configs

--- config_runners/run_pretrain_rassp1_neims1_200peaks.sh
+++ config_runners/run_pretrain_rassp1_neims1_200peaks.sh
@@ -1,5 +1,5 @@
 # RASSP pretrain from scratch
-python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_200peaks.yaml \
+python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_200peaks_masks.yaml \
                         --additional-info "_rassp1_neims1_200peaks" \
                         --additional-tags "A100:scratch:RASSP1NEIMS1:meta" \
                         --wandb-group pretrain \
