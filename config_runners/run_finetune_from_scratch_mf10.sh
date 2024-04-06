cc43132f696f830be0c873cab65207b9458735ca
hejjack <hejjack1@gmail.com>
2024-04-06 12:18:25 +0200

    update / add configs

--- config_runners/run_finetune_from_scratch_mf10.sh
+++ config_runners/run_finetune_from_scratch_mf10.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python ../train_bart.py --config-file ../configs/train_config_finetune_mf10.yaml \
-                                               --additional-info _from_scratch_mf10 \
-                                               --wandb-group finetune \
-                                               --additional-tags "A100:alfa:mf10"
+python ../train_bart.py --config-file ../configs/train_config_finetune_mf10.yaml \
+                        --additional-info _from_scratch_mf10 \
+                        --wandb-group finetune \
+                        --additional-tags "H100:meta:mf10"
