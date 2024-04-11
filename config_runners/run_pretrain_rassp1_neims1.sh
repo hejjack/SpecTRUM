8a473342f8d01ea669caa89b0a0196514b789693
Adam Hajek <hejjack1@gmail.com>
2024-04-11 12:11:34 +0200

    inspect nist mol weight

--- config_runners/run_pretrain_rassp1_neims1.sh
+++ config_runners/run_pretrain_rassp1_neims1.sh
@@ -1,7 +1,7 @@
 # RASSP pretrain from scratch
-python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_224k.yaml \
-                        --additional-info "_rassp1_neims1" \
-                        --additional-tags "H100:scratch:rassp1:neims1:meta" \
-                        --wandb-group pretrain \
-                        --checkpoint ../checkpoints/pretrain/blooming-thunder-439_rassp1_neims1/checkpoint-112000 \
-                        --resume-id "ai0xxvoh"
+CUDA_VISIBLE_DEVICES=0 python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_224k.yaml \
+                                            --additional-info "_rassp1_neims1" \
+                                            --additional-tags "A100:scratch:rassp1:neims1:alfa" \
+                                            --wandb-group pretrain \
+                                            --checkpoint ../checkpoints/pretrain/blooming-thunder-439_rassp1_neims1/checkpoint-112000 \
+                                            --resume-id "ai0xxvoh"
