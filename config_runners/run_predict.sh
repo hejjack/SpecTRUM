e6f57814ddc493ce30e3feb29f5a359be7dcb1d4
hejjack <hejjack1@gmail.com>
2024-04-17 16:25:31 +0200

    add new predictions

--- config_runners/run_predict.sh
+++ config_runners/run_predict.sh
@@ -1,4 +1,4 @@
 python ../predict.py --checkpoint ../checkpoints/finetune/legendary-disco-449_rassp1_neims1_nist01_224k/checkpoint-73738 \
                     --output-folder ../predictions \
-                    --config-file ../configs/predict_config_nist_valid.yaml \
+                    --config-file ../configs/predict_config_nist_valid_beam50.yaml \
                                             
