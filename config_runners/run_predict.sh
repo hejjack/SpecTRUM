db657548e79a77e30e8d3a7927584b10aca765fe
hejjack <hejjack1@gmail.com>
2024-05-16 13:13:05 +0200

    greedy decoding on test

--- config_runners/run_predict.sh
+++ config_runners/run_predict.sh
@@ -1,4 +1,4 @@
 python ../predict.py --checkpoint ../checkpoints/finetune/fearless-wildflower-490_rassp1_neims1_224kPretrain_148k/checkpoint-147476 \
                     --output-folder ../predictions \
-                    --config-file ../configs/predict_config_nist_test.yaml \
+                    --config-file ../configs/predict_config_nist_test_greedy.yaml \
                                             
