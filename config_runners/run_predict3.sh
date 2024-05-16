c1b5317fbe54ca4314cc3b65266ea062f256e8c4
hejjack <hejjack1@gmail.com>
2024-05-16 13:02:48 +0200

    new test/valid predictions

---
+++ config_runners/run_predict3.sh
@@ -0,0 +1,3 @@
+python ../predict.py --checkpoint ../checkpoints/finetune/fearless-wildflower-490_rassp1_neims1_224kPretrain_148k/checkpoint-147476 \
+                     --output-folder ../predictions \
+                     --config-file ../configs/predict_config_nist_valid.yaml \
