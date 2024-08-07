503382a0ce4a0be818f48ebafbc2441008f4c58c
Ales Krenek <ljocha@ics.muni.cz>
2024-08-07 18:12:08 +0200

    filter non-digestible moledules

--- clean_paper/forward/rassp-pbs.sh
+++ clean_paper/forward/rassp-pbs.sh
@@ -35,8 +35,15 @@ if [ -n "$model" ]; then
 	cp "$model" $SCRATCHDIR/formulanet.model
 	cp "$meta" $SCRATCHDIR/formulanet.meta
 else
-	curl -k -o $SCRATCHDIR/formulanet.model 'https://people.cs.uchicago.edu/~ericj/rassp/formulanet_best_candidate_pcsim_pretrain.nist-fromscratch-3x9x128.35790555.00000740.model' 
-	curl -k -o $SCRATCHDIR/formulanet.meta 'https://people.cs.uchicago.edu/~ericj/rassp/formulanet_best_candidate_pcsim_pretrain.nist-fromscratch-3x9x128.35790555.meta' 
+	md5sum -c - <<EOF
+fac1e8124104363cfaec2dc5d0b93046  $SCRATCHDIR/formulanet.meta
+3c19262f6fbcb5928afc9b7a35e0cd33  $SCRATCHDIR/formulanet.model
+EOF
+
+	if [ $? != 0 ]; then
+		curl -k -o $SCRATCHDIR/formulanet.model 'https://people.cs.uchicago.edu/~ericj/rassp/formulanet_best_candidate_pcsim_pretrain.nist-fromscratch-3x9x128.35790555.00000740.model' 
+		curl -k -o $SCRATCHDIR/formulanet.meta 'https://people.cs.uchicago.edu/~ericj/rassp/formulanet_best_candidate_pcsim_pretrain.nist-fromscratch-3x9x128.35790555.meta' 
+	fi
 fi
 
 cp $(dirname $0)/rassp-predict.py $SCRATCHDIR
@@ -50,19 +57,27 @@ if [ -n "$CUDA_VISIBLE_DEVICES" ]; then
 fi
 
 chunk=$1
-split -d -l 1000 ${chunk} ${chunk}_
+base=$(basename $chunk)
 
-for c in ${chunk}_900*; do
-	mv $c $(echo $c | sed 's/900\(.\)/9\1/')
+split -d -l 1000 ${chunk} ${base}_
+
+# naming hack to keep _NN on two digits only
+#for c in ${chunk}_900*; do
+#	mv $c $(echo $c | sed 's/900\(.\)/9\1/')
+#done
+
+rm -f *.jsonl *-discard.smi
+
+for c in ${base}_*; do
+	singularity exec $nv -B $PWD:/work --pwd /work "$image" /opt/nvidia/nvidia_entrypoint.sh python3 rassp-predict.py -m formulanet.model -e formulanet.meta -w ${PBS_NCPUS:-1} -s "$c" -o "$c.jsonl" -d "$c-discard.smi"
 done
 
-rm -f *.jsonl
-for c in ${chunk}_*; do
-	singularity exec $nv -B $PWD:/work --pwd /work "$image" /opt/nvidia/nvidia_entrypoint.sh python3 rassp-predict.py -m formulanet.model -e formulanet.meta -w ${PBS_NCPUS:-1} -s "$c" -o "$c.jsonl"
-done
+obase=$(echo $chunk | sed 's/\.[^/.]*$//')
 
-cat *.jsonl >"$dir/$1.jsonl"
+cat *.jsonl >"$obase.jsonl"
+cat *-discard.smi >"$obase-discard.smi"
 
+# cleanup if needed
 # rm -rf *
 
 
