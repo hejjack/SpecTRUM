c08f0b76c1e442f2503841a97d716bcffc63eaa6
Adam Hajek <hejjack1@gmail.com>
2024-09-09 21:42:12 +0200

    change first runscript

--- config_runners/run_pretrain_rassp1_neims1.sh
+++ config_runners/run_pretrain_rassp1_neims1.sh
@@ -1,5 +1,5 @@
 # RASSP pretrain from scratch
-python ../train_bart.py --config-file ../configs/train_config_pretrain_rassp1_neims1_40bin.yaml \
-                        --additional-info "_rassp1_neims1_40bin" \
-                        --additional-tags "H100:scratch:rassp1:neims1:capy" \
-                        --wandb-group pretrain \
+python ../train_bart.py --config-file ../clean_paper/configs/pretrain_exp4_rassp_neims.yaml \
+                        --additional-info "_exp4_rassp_neims" \
+                        --additional-tags "exp4:rassp:neims:from_scratch" \
+                        --wandb-group pretrain_clean \
